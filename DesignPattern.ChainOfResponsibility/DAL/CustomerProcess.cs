dbea2810211796534e49d02806c96b80fee21260
mustafaguler3 <mustafa.glr@hotmail.com>
2023-05-18 13:01:31 +0300

    initial

---
+++ DesignPattern.ChainOfResponsibility/DAL/CustomerProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class CustomerProcess
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string EmployeeName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DesignPattern.ChainOfResponsibility/DAL/VtContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class VtContext : DbContext
+    {
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-N81IS28;database=designPattern1;integrated security=true");
+        }
+    }
+}
